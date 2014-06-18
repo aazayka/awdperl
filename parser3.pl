c1d263c6ac446e541fb6fbb302dbe94c514331c5
aazayka <aazayka@mail.ru>
2014-06-18 21:06:46 +0400

    Working version

--- parser3.pl
+++ parser3.pl
@@ -9,30 +9,34 @@ my $html = get("http://forum.awd.ru/viewforum.php?f=60")
 use HTML::TokeParser;
 open (MYFILE, '>data.txt');
 my $p = HTML::TokeParser->new( \$html );
+my $topic_link;
+my $topic_name;
+my $check_link;
 
-while (my $token = $p->get_tag("dt")) {
-    print MYFILE "DT START\n";
+while (my $token = $p->get_tag("dt", "a", "span")) {
+	if ($token->[0] eq "dt") {
+		$topic_link = "";
+		$topic_name = "";
+		$check_link = 0;
+	} elsif ($token->[0] eq "a" && $token->[1]{class} eq "topictitle") {
+		$topic_link = $token->[1]{href};
+		$topic_name = $p->get_trimmed_text("/a");
+		$check_link = 1;
+	} elsif ($check_link && $token->[0] eq "span" && $token->[1]{class} eq "left-box") {
+		my $span_text = $p->get_trimmed_text("/span");
+		if (
+				index($span_text, "Сегодня") != -1
+			 || index($span_text, "минут") != -1
+			 || index($span_text, "секунд") != -1
+			 || index($span_text, "Вчера") != -1 
+		) 
+		{
+			print MYFILE "$span_text: $topic_name  - $topic_link\n";
+			$topic_link = "";
+			$topic_name = "";
+		}
 
-    while (my $links = $p->get_tag("a")) {
-      print MYFILE "  LINKS START\n";
-      if ($links->[1]{class} eq "topictitle") {
-        my $topic_name = $p->get_trimmed_text("/a");
-        my $topic_link = $links->[1]{href};
-
-        while (my $dates = $p->get_tag("span")) {
-          if ($dates->[1]{"class"} eq "left-box") {
-		    my $span_text = $p->get_trimmed_text("/span");
-            if (index($span_text, "Вчера") != -1 || index($span_text, "Сегодня") != -1) {
-              print MYFILE $topic_name || ' - ' || $topic_link;
-              print MYFILE "\n";
-            }
-          }
-          print MYFILE "    span end\n";
-        }
-      }
-     print MYFILE "  LINKS end\n";
-    }
-  print MYFILE "TD end\n";
+	}
 }
 
 close (MYFILE);
