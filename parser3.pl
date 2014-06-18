4ffc04e02ae7e07805abb1a1ea829f7d88428b2b
aazayka <aazayka@mail.ru>
2014-06-18 19:18:54 +0400

    Более-менее рабочий вариант

--- parser3.pl
+++ parser3.pl
@@ -4,11 +4,44 @@ use LWP::Simple;
 my $html = get("http://forum.awd.ru/viewforum.php?f=60")
            or die "Could not fetch NWS CSV page.";
 
- use HTML::TokeParser::Simple;
- my $p = HTML::TokeParser::Simple->new( $html );
+use HTML::TokeParser;
+open (MYFILE, '>data.txt');
+my $p = HTML::TokeParser->new( \$html );
 
- while ( my $token = $p->get_token ) {
-     # This prints all text in an HTML doc (i.e., it strips the HTML)
-     next unless $token->is_text;
-     print $token->as_is;
- }
+while (my $token = $p->get_tag("dt")) {
+    print MYFILE "DT START\n";
+
+    while (my $links = $p->get_tag("a")) {
+      print MYFILE "  LINKS START\n";
+      if ($links->[1]{class} eq "topictitle") {
+        my $topic_name = $p->get_trimmed_text("/a");
+        my $topic_link = $links->[1]{href};
+
+        while (my $dates = $p->get_tag("span")) {
+          print MYFILE "    SPAN START\n";
+          print $dates->[0];
+          print "\n";
+          print $dates->[1];
+          print "\n";
+          print $dates->[2];
+          print "\n";
+          print $dates->[3];
+          print "\n";
+
+          if ($dates->[1]{"class"} eq "left-box") {
+            print MYFILE $p->get_text("/span");
+            print MYFILE "\n";
+          }
+          print MYFILE "    span end\n";
+        }
+      }
+      print MYFILE "  LINKS end\n";
+    }
+  print MYFILE "TD end\n";
+
+#    my $url = $token->[1]{href} || "-";
+#    my $text = $p->get_trimmed_text("/a");
+#    print "$url\t$text\n";
+}
+
+close (MYFILE);
